ae44f619cbd6f2388b5fb29c01295630dfed9546
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-15 09:11:53 +0100

    Update ChetnyeChisla.java

--- src/main/java/org/example/lec_2/task_6/ChetnyeChisla.java
+++ src/main/java/org/example/lec_2/task_6/ChetnyeChisla.java
@@ -1,11 +1,11 @@
 package org.example.lec_2.task_2;
 
-public class ChetnyeChisla {
+public class ChetnyeChisla { // ChetnyeChisla - названия стоит делать на англ языке, а не транслитом, переводчик тебе в этом поможет
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++)
             if (i % 2 == 0) {
                 System.out.println(i);
-
+//эта строка не нужна
             }
     }
 }
