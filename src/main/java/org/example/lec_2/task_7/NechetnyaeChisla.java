8f579352f0fb750c3620693e2685fdfe21755de7
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-15 09:12:45 +0100

    Update NechetnyaeChisla.java

--- src/main/java/org/example/lec_2/task_7/NechetnyaeChisla.java
+++ src/main/java/org/example/lec_2/task_7/NechetnyaeChisla.java
@@ -1,2 +1,2 @@
-package org.example.lec_2.task_7;public class NechetnyaeChisla {
+package org.example.lec_2.task_7;public class NechetnyaeChisla { // NechetnyaeChisla - имена классов стоит делать на англ языке, а не транслитом, переводчик тебе в этом поможет
 }
